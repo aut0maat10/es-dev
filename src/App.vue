5600f469fd64c49113fc9d10df3efb24b0982fa7
Robin <robin.pahlman@gmail.com>
2023-02-10 13:49:42 -0500

    basic setup

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import Hero from './components/Hero.vue'
+import NavBar from './components/NavBar.vue'
+</script>
+
+<template>
+  <NavBar />
+  <Hero  />
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/Hero.vue
@@ -0,0 +1,18 @@
+<script setup>
+</script>
+
+<template>
+  <div class="hero min-h-screen bg-[url('src/assets/office.jpg')]">
+  <div class="hero-overlay bg-opacity-60"></div>
+  <div class="hero-content text-center text-neutral-content">
+    <div class="max-w-md">
+      <h1 class="mb-5 text-5xl font-bold">Creative solutions for small businesses</h1>
+      <p class="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+      <button class="btn btn-primary">Get Started</button>
+    </div>
+  </div>
+</div>
+</template>
+
+<style scoped>
+</style>
