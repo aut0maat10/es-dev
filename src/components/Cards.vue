5cca962c41f86f265eec654cfa9e756390af5083
Robin <robin.pahlman@gmail.com>
2023-02-15 15:56:18 -0500

    add test graphics

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex md:flex-row flex-wrap justify-center gap-10">
     <div class="card w-96 bg-base-100 shadow-xl">
-      <figure><img src="src/assets/graphic.png" alt="Shoes" /></figure>
+      <figure><img src="src/assets/graphic2.png" alt="Shoes" /></figure>
       <div class="card-body">
         <h2 class="card-title">Shoes!</h2>
         <p>If a dog chews shoes whose shoes does he choose?</p>
@@ -12,7 +12,7 @@
     </div>
     <!-- card 2 -->
     <div class="card w-96 bg-base-100 shadow-xl">
-      <figure><img src="src/assets/graphic.png" alt="Shoes" /></figure>
+      <figure><img src="src/assets/graphic3.png" alt="Shoes" /></figure>
       <div class="card-body">
         <h2 class="card-title">Shoes!</h2>
         <p>If a dog chews shoes whose shoes does he choose?</p>
