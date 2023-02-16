186d80f1864b11554c3fed5afeb3faaad382b5f7
Robin <robin.pahlman@gmail.com>
2023-02-16 11:14:19 -0500

    card refactor

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -1,46 +1,29 @@
 <template>
   <div class="flex md:flex-row flex-wrap justify-center gap-10 py-8">
-    <div class="card w-72 bg-base-100 shadow-xl py-6">
-      <figure><img src="src/assets/social-sharing.svg" alt="Laptop" /></figure>
+    <div v-for="(card, index) in cards" :key="index" class="card w-72 bg-base-100 shadow-xl py-6">
+      <figure><img :src="`src/assets/${card.img}.svg`" alt="Laptop" /></figure>
       <div class="card-body">
-        <h2 class="card-title">Shoes!</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
-        <div class="card-actions justify-end">
+        <h2 class="card-title">{{ card.title }}</h2>
+        <p>{{ card.text }}</p>
+        <!-- <div class="card-actions justify-end">
           <button class="btn btn-primary">Buy Now</button>
-        </div>
-      </div>
-    </div>
-    <!-- card 2 -->
-    <div class="card w-72 bg-base-100 shadow-xl py-6">
-      <figure><img src="src/assets/projections.svg" alt="Shoes" /></figure>
-      <div class="card-body">
-        <h2 class="card-title">Shoes!</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
-        <div class="card-actions justify-end">
-          <button class="btn btn-primary">Buy Now</button>
-        </div>
-      </div>
-    </div>
-    <!-- card 3 -->
-    <div class="card w-72 bg-base-100 shadow-xl py-6">
-      <figure><img src="src/assets/graphic5.svg" alt="Shoes" /></figure>
-      <div class="card-body">
-        <h2 class="card-title">Shoes!</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
-        <div class="card-actions justify-end">
-          <button class="btn btn-primary">Buy Now</button>
-        </div>
+        </div> -->
       </div>
     </div>
   </div>
 </template>
 
 <script>
+const cards = [
+  {title: "title 1", text: "lorem ipsum dolor sit amet", img: "social-sharing"},
+  {title: "title 2", text: "lorem ipsum dolor sit amet", img: "projections"},
+  {title: "title 3", text: "lorem ipsum dolor sit amet", img: "graphic5"}
+]
 export default {
   setup () {
 
 
-    return {}
+    return {cards}
   }
 }
 </script>
